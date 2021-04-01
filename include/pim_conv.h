ecb3d7a325054cda5f00f69d29919d5e3d954ffc
zebfff <wubin200@126.com>
2021-04-01 21:57:58 +0800

    add pim_array_pro in pim_conv.h pim_linear.h

--- include/pim_conv.h
+++ include/pim_conv.h
@@ -409,6 +409,11 @@ namespace PIM {
               wb_ptr, wb_t_ptr, prev_ptrs,
               input, weight, options.bias() ? bias : c10::optional<Tensor>(),
               options.stride(), options.padding(), is_training_);
+        case PimArrayType::pim_array_pro:
+          return PimConv2dFunction<pimArrayPro>::apply(
+              wb_ptr, wb_t_ptr, prev_ptrs,
+              input, weight, options.bias() ? bias : c10::optional<Tensor>(),
+              options.stride(), options.padding(), is_training_);
         default:
           TORCH_INTERNAL_ASSERT(false, "pim conv, forward type not supported!")
       }

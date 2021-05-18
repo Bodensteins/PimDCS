39656fe0f4bbe4d92f60417a1430480314c4b684
zebfff <wubin200@126.com>
2021-05-18 17:13:14 +0800

    delete pim_array config

--- include/pim_array_config.h
+++ include/pim_array_config.h
@@ -7,51 +7,6 @@
 #include <cmath>
 using torch::indexing::Slice;
 
-struct pim_array_config
-{
-    int32_t rowSize, colSize;                       //  logic array size
-    int32_t phyArrRowSize, phyArrColSize;           //  phy array size, a logic array is formed by one or multiple phy arrays.
-    int32_t inBits, inVBits, outBits, unitBits, cellBits;    /*  input/output data bits.  unit bits means precision of data in array. cell bits means one memory cell's precision
-                                                        e.g. unitBits = 8, cellBits = 2.  we need 4 memory cell to represent 1 unit.
-                                                   */
-
-    bool has_negative_input;                        // input value has negative number
-    double max_phy_input_value;                     // input value has its maximum, we will use this maximum to regionalizatoin input value by inBits.
-    double max_weight_value;                        // weight value has its maximum, we will use this maximum to regionalizatoin weight value by unitBits.
-    bool trunc_input;                               // if true, the value > max_phy_input_value, will trunc to the max_phy_input_value. if false, if will reprot error if value>max_phy_input
-    bool dynamic_max_input;                         // if true, we will dynamic get max_input rather than use max_phy_input_value
-    
-    pim_array_config(std::string filename = "../config/pimarray.yaml")
-    {
-        YAML::Node config = YAML::LoadFile(filename);
-        rowSize = config["rowSize"].as<int>();
-        colSize = config["colSize"].as<int>();
-        phyArrRowSize = config["phyArrRowSize"].as<int>();
-        phyArrColSize = config["phyArrColSize"].as<int>();
-        inBits = config["inBits"].as<int>();
-        outBits = config["outBits"].as<int>();
-        unitBits = config["unitBits"].as<int>();
-        cellBits = config["cellBits"].as<int>();
-        has_negative_input = config["has_negative_input"].as<bool>();
-        max_phy_input_value = config["max_phy_input_value"].as<double>();
-        max_weight_value = config["max_weight_value"].as<double>();
-        trunc_input = config["trunc_input"].as<bool>();
-        dynamic_max_input = config["dynamic_max_input"].as<bool>();
-    }
-
-    void print(std::ostream & os)
-    {
-        os << "phyArr sizes = [" << phyArrRowSize << ',' << phyArrColSize << "]\n" \
-            << "in/out/unit bits" "= [" << inBits << '/' << outBits << '/' << unitBits << "]\n" \
-            << "has neg " << has_negative_input << ',' << "max_phy_input =" << max_phy_input_value \
-            << "trunc_input " << trunc_input << "dynamic_max " << dynamic_max_input << std::endl;
-    }
-};
-
-
-const pim_array_config decf("../config/pimarray.yaml");
-
-const pim_array_config decf_for_counters("../config/pim_onlyCounter.yaml");
 
 enum struct phy_array_writeMode
 {
@@ -66,9 +21,6 @@ enum struct phy_array_readMode
     V_Div_3,
     Ground
 };
-
-
-
 
 struct pim_array_pro_config
 {

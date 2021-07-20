76fc593a61a218139a76a714a977766784e52479
Cheng Huan <1556673504@qq.com>
2021-07-20 15:13:46 +0800

    complete mmtest.cpp

--- unittest/mmtest.cpp
+++ unittest/mmtest.cpp
@@ -5,7 +5,6 @@
 #include "pim_array_example.h"
 #include "pim_array_config.h"
 #include <map>
-#include <ctime>
 #include <vector>
 #include <cmath>
 #include <algorithm>
@@ -16,58 +15,53 @@ using std::endl;
 using std::pair;
 using std::make_pair;
 
-pair<double ,double> mvTest(double &speedUp);
-double get_err(const at::Tensor &std_out, const at::Tensor &out1);
+pair<int ,int> mvTest();
+int getMaxPrecision(const at::Tensor &stdOut, const at::Tensor &myOut);
+void printPrecision(int index);
 
 int main()
 {
     int testNum = 100;
-    //int errnum = 0;
-    double errmy = 0;
-    double errtra = 0;
-    double speedSum = 0;
-    double speedUp;
-    vector<double> spp;
-    pair<double, double> err;
+
+    int errLowADC = 100;
+    int errHighADC = 100;
+
+    pair<int, int> err;
     for (int i = 0; i < testNum; ++i)
     {
         cout << "case " << i  << ':' << endl;
-        err = mvTest(speedUp);
-        errmy += err.first;
-        errtra += err.second;
-        spp.push_back(speedUp);
-        //errnum += (err > 5);
+        err = mvTest();
+        if (err.first < errLowADC)
+        {
+            errLowADC = err.first;
+        }
+        if (err.second < errHighADC)
+        {
+            errHighADC = err.second;
+        }
     }
-    std::sort(spp.begin(), spp.end());
-
-    for (int i=10; i<testNum-10; ++i)
-        speedSum += spp[i];
 
     cout << endl << "final result:" << endl;
-    cout << "our average error rate: " << errmy/testNum << "%" << endl;
-    cout << "traditional average error rate: " << errtra/testNum << "%" << endl;
-    cout << "speed up: " << speedSum/(testNum-20) << endl;
+    cout << "low adc precision: " << endl;
+    printPrecision(errLowADC);
+    cout << "high adc precision: " << endl;
+    printPrecision(errHighADC);
 
     return 0;
 }
 
-pair<double ,double> mvTest(double &speedUp)
+pair<int ,int> mvTest()
 {
-    pim_array_pro_config config("../config/5_2_4.yaml");
-    pim_array_pro_config cfg_traditional("../config/5_2_4_traditional.yaml");
-    double computeTime = config.latency.phyMMLatency * 1e-9;
+    pim_array_pro_config config_low("../config/lowadc.yaml");
+    pim_array_pro_config config_high("../config/highadc.yaml");
 
-    double softwareTime;
-    double ourTime;
-    //double speedUp;
-
-    const int M = 512;  // array of M x N
-    const int N = 512;  //
+    const int M = 8;  // array of M x N
+    const int N = 8;  //
 
     auto op = torch::TensorOptions(torch::kCPU).dtype(torch::kFloat64);
 
-    pimArrayPro myArray(M, N, op, &config);
-    pimArrayPro traditionalArray(M, N, op, &cfg_traditional);
+    pimArrayPro lowArray(M, N, op, &config_low);
+    pimArrayPro highArray(M, N, op, &config_high);
 
     SimpleLogicArray softwareArray(M, N, op);
     at::Tensor v;
@@ -76,85 +70,74 @@ pair<double ,double> mvTest(double &speedUp)
     writemat = writemat.div(writemat.abs().max());
 
     softwareArray.write_mat(writemat);
-    myArray.write_mat(writemat);
-//    cout << "myarray:" << endl;
-//    myArray.print(cout);
-    traditionalArray.write_mat(writemat);
-//    cout << "traditional:" << endl;
-//    traditionalArray.print(cout);
+    cout << "truearray:" << endl;
+    cout << softwareArray.read_mat() << endl;
+    lowArray.write_mat(writemat);
+    cout << "lowarray:" << endl;
+    cout << lowArray.read_mat() << endl;
+    highArray.write_mat(writemat);
+    cout << "higharray:" << endl;
+    cout << highArray.read_mat() << endl;
 
     v = at::randn({1, M}, torch::kFloat64);
     v.div_(v.abs().max());
 
     //cout << v << endl;
-
-    auto st = clock();
     auto out = softwareArray.mm(v);
-    auto ed = clock();
-    softwareTime = (double)(ed - st) / CLOCKS_PER_SEC;
-    cout << "software implemention time = " << softwareTime << endl;
 
-    cout << "my:" << endl;
-    auto out1 = myArray.mm(v);
-    ourTime = config.inBits/config.inVBits * computeTime;
-    cout << "our time = " << ourTime << endl;
+    //cout << "low:" << endl;
+    auto lowout = lowArray.mm(v);
 
-    cout << "traditional:" << endl;
-    auto out2 = traditionalArray.mm(v);
-
-    speedUp = softwareTime / ourTime;
-    cout << "speedup: " << speedUp << endl;
+    //cout << "high:" << endl;
+    auto highout = highArray.mm(v);
 
     out = out.squeeze();
-    out1 = out1.squeeze();
-    out2 = out2.squeeze();
+    lowout = lowout.squeeze();
+    highout = highout.squeeze();
 
-    //cout << "out:" << endl << out << "out1:" << endl <<  out1 << "out2:" << endl << out2 << endl;
+    cout << "true out:" << endl << out  << endl << "low out:" << endl << lowout  << endl << "high out:" << endl << highout << endl;
 
-    double error = get_err(out, out1);
+    int err1 = getMaxPrecision(out, lowout);
     //cout << "out1 end" << endl << endl;
-    double err2 = get_err(out, out2);
-    cout << "error rate: " << error << "%" << " traditional error rate: " << err2 << "%" << endl;
+    int err2 = getMaxPrecision(out, highout);
+    //cout << "low adc precision bit: " << err1 << " high adc precision bit: " << err2 << endl;
 
-    return make_pair(error, err2);
+    return make_pair(err1, err2);
 }
 
-double get_err(const at::Tensor &out, const at::Tensor &out1)
-{
-    double epsilon = 1e-3;
-    auto newout = out.clone();
-
-    for (int i = 0; i < out.size(0); ++i)
-    {
-        double outi = out[i].item<double>();
-        double out1i = out1[i].item<double>();
-
-        if (fabs(outi) < epsilon)
-        {
-            newout[i] = epsilon;
-        }
-
-        if (fabs(out1i - outi)/fabs(newout[i].item<double>()) > 10)
-        {
-            //cout << "out[" << i <<  "] = " << outi  << endl << "out1[" << i << "] = " << out1i << endl;
-        }
-    }
-
-    return (out - out1).div(newout).abs().mean().item<double>() * 100;
-}
-
-double getMaxPrecision(const at::Tensor &out, const at::Tensor &out1)
+int getMaxPrecision(const at::Tensor &out, const at::Tensor &out1)
 {
     auto delta = out - out1;
     double maxDistance  = delta.abs().max().item<double>();
 
-    int n = 1;
+    int n = 10;
     for (int i = 0; ; ++i, n *= 10)
     {
         if (n * maxDistance >= 1)
         {
             return i;
         }
+    }
+}
+
+void printPrecision(int index)
+{
+    if (index < 0)
+    {
+        cout << "error! illegal precision!" << endl;
+    }
+    else if (index == 0)
+    {
+        cout << "1" << endl;
+    }
+    else
+    {
+        cout << "0.";
+        for (int i = 0; i < index - 1; ++i)
+        {
+            cout << "0";
+        }
+        cout << "1" << endl;
     }
 }
 

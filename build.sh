07e302102df64db0a9e159993805bfab416ba8f0
Bing Wu <wubin200@126.com>
2021-09-24 16:46:51 +0800

    delete all test file. only include is left.

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ if [ ! -d "$build_dir" ]; then
 fi
 
 #TORCH_LIBRARY=/home/bing/SSD/softwares/libtorch_gpu-1.9/libtorch
-TORCH_LIBRARY=/etc/libtorch
+TORCH_LIBRARY=/opt/libtorch
 cd $build_dir
 
 cmake .. -DCMAKE_PREFIX_PATH=$TORCH_LIBRARY -DCMAKE_BUILD_TYPE=Debug

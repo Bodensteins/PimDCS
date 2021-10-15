b6b00bb1a0651aa691c09ccce9f6466d1915d82c
chenghuan <1556673504@qq.com>
2021-10-15 11:25:27 +0800

    modify interface

--- pimfixedpoint/pimlinear.py
+++ pimfixedpoint/pimlinear.py
@@ -3,7 +3,9 @@ import torch
 import math
 from torch.autograd import Function, grad
 from torch import Tensor
-from quantization import NormalTensor, ArrayTensor, RefTensor
+from quantization import add_additional_one, change_bit_width_, write_array, normal_matmul_array, \
+    remove_additional_one_, normal_t_matmul_array, TensorType, creat_quantization_para, quantization_tensor, \
+    parse_quantization_para, de_quantization, quantization_tensor_less
 
 class PimLinearFunction(Function):
     @staticmethod
@@ -11,11 +13,15 @@ class PimLinearFunction(Function):
         qweight_config: Tensor, qweight_t: Tensor, qweight_t_config: Tensor, inputBits: int, gradOutputBits: int, hasBias: bool):
 
         if hasBias:
-            add_additional_one_([qinput, qinput_config])
+            # add_additional_one_([qinput, qinput_config])
+            qinput = add_additional_one([qinput, qinput_config])
         
-        chenge_bit_width_([qinput, qinput_config], inputBits)
-        write_array_([qinputArr, qinputArr_config], [qinput, qinput_config])
-        qoutput, qoutput_config = pim_matmul([qinput, qinput_config], [qweight, qweight_config])
+        # chenge_bit_width_([qinput, qinput_config], inputBits)
+        change_bit_width_([qinput, qinput_config], inputBits)
+        # write_array_([qinputArr, qinputArr_config], [qinput, qinput_config])
+        qinputArr = write_array([qinputArr, qinputArr_config], [qinput, qinput_config])
+        # qoutput, qoutput_config = pim_matmul([qinput, qinput_config], [qweight, qweight_config])
+        qoutput, qoutput_config = normal_matmul_array([qinput, qinput_config], [qweight, qweight_config])
 
         ctx.save_for_backward(qinputArr, qweight_t)
         ctx.qinputArr_config = qinputArr_config
@@ -37,10 +43,16 @@ class PimLinearFunction(Function):
 
         change_bit_width_([qgrad_output, qgrad_output_config], qgrad_output_bits)
         
-        qgrad_input, qgrad_input_config = pim_matmul([qgrad_output, qgrad_output_config], [qweight_t, qweight_t_config])
+        # qgrad_input, qgrad_input_config = pim_matmul([qgrad_output, qgrad_output_config], [qweight_t, qweight_t_config])
+        qgrad_input, qgrad_input_config = normal_matmul_array([qgrad_output, qgrad_output_config], [qweight_t, qweight_t_config])
+
         if hasBias:
-            remove_additional_one_([qgrad_input, qgrad_input_config])
-        delta_qweight_t = pim_t_matmul([qgrad_output, qgrad_output_config], [qinputArr, qinputArr_config])
+            # remove_additional_one_([qgrad_input, qgrad_input_config])
+            remove_additional_one_(qgrad_input)
+
+        # delta_qweight_t = pim_t_matmul([qgrad_output, qgrad_output_config], [qinputArr, qinputArr_config])
+        # need quantization para?
+        delta_qweight_t, _ = normal_t_matmul_array([qgrad_output, qgrad_output_config], [qinputArr, qinputArr_config])
 
         return qgrad_input, qgrad_input_config, None, None, None, delta_qweight_t, None, None, None, None
 
@@ -58,15 +70,19 @@ class PimLinear(torch.nn.Module):
         self.wArr, self.wtArr, self.inputArr = None, None, None
 
         if arrayMode == "RefTensor":
-            self.inputArrConfig = initRefArrayConfig(weightBits)
-            self.wArrConfig = initRefArrayConfig(weightBits)
-            self.wtArrConfig = initRefArrayConfig(weightBits)
+            # self.inputArrConfig = initRefArrayConfig(weightBits)
+            # self.wArrConfig = initRefArrayConfig(weightBits)
+            # self.wtArrConfig = initRefArrayConfig(weightBits)
+            self.inputArrConfig = creat_quantization_para(bit_width=weightBits, tensor_type=TensorType.Ref)
+            self.wArrConfig = creat_quantization_para(bit_width=weightBits, tensor_type=TensorType.Ref)
+            self.wtArrConfig = creat_quantization_para(bit_width=weightBits, tensor_type=TensorType.Ref)
         # elif arrayMode == "PNTensor":
         #    self.wArr = PNTensor(m, n, absMaxValue, bitwidth)
         #    self.wtArr = PNTensor(n, m, absMaxValue, bitwidth) 
         #    self.inputArr = PNTensor(batch_size, m, absMaxValue, bitwidth)
         else:
-            print("arrayMode error!")
+            # print("arrayMode error!")
+            raise Exception("We don't implement this array mode!", arrayMode)
 
         self.weight_init()
     
@@ -79,8 +95,10 @@ class PimLinear(torch.nn.Module):
             bound = 1 / math.sqrt(fan_in)
             torch.nn.init.uniform_(temp_weight[-1], -bound, bound)
 
-        pim_arr_quantization([self.wArr, self.wArrConfig], temp_weight, max(self.maxValue, temp_weight.abs().max()))
-        pim_arr_quantization([self.wtArr, self.wtArrConfig], temp_weight.t(), max(self.maxValue, temp_weight.abs().max()))
+        # pim_arr_quantization([self.wArr, self.wArrConfig], temp_weight, max(self.maxValue, temp_weight.abs().max()))
+        # pim_arr_quantization([self.wtArr, self.wtArrConfig], temp_weight.t(), max(self.maxValue, temp_weight.abs().max()))
+        self.wArr = quantization_tensor(self.wArrConfig, temp_weight, max(self.maxValue, temp_weight.abs().max()))
+        self.wtArr = quantization_tensor(self.wtArrConfig, temp_weight.t(), max(self.maxValue, temp_weight.abs().max()))
 
     def forward(self, qinput: Tensor, qinput_config):
         qoutput, qoutput_config = PimLinearFunction.apply(qinput, qinput_config, self.inputArr, self.inputArrConfig, self.wArr, self.wArrConfig, self.wtArr, 
@@ -93,16 +111,20 @@ class DeQuanFunction(Function):
     @staticmethod
     def forward(ctx, qinput: Tensor, qinput_config: Tensor, bit: int, quantizerMode: str):
         if quantizerMode == "dynamic":
-            ctx.bit = get_bit(qinput_config)
+            # ctx.bit = get_bit(qinput_config)
+            _, ctx.bit, _ = parse_quantization_para(qinput_config)
         else:
             ctx.bit = bit
-        return de_quantization(qinput, qinput_config)
+        # return de_quantization(qinput, qinput_config)
+        return de_quantization([qinput, qinput_config])
 
     @staticmethod
     def backward(ctx, grad_output: Tensor):
-        qgrad_output_config = initNormalConfig(ctx.bit)
-        qgrad_output = torch.empty(grad_output.size())
-        pim_normal_quantization([qgrad_output, qgrad_output_config], grad_output, grad_output.abs().max())
+        # qgrad_output_config = initNormalConfig(ctx.bit)
+        qgrad_output_config = creat_quantization_para(bit_width=ctx.bit, tensor_type=TensorType.Normal)
+        # qgrad_output = torch.empty(grad_output.size())
+        # pim_normal_quantization([qgrad_output, qgrad_output_config], grad_output, grad_output.abs().max())
+        qgrad_output = quantization_tensor(qgrad_output_config, grad_output, grad_output.abs().max())
         return qgrad_output, qgrad_output_config, None, None
 
 
@@ -119,9 +141,10 @@ class DeQuanLayer(torch.nn.Module):
 class ReluFunction(Function):
     @staticmethod
     def forward(ctx, qinput: Tensor, qinput_config: Tensor):
-        neg_position = pim_le([qinput, qinput_config], 0)
+        # neg_position = pim_le([qinput, qinput_config], 0)
+        neg_position = quantization_tensor_less([qinput, qinput_config], 0)
         ctx.neg_position = neg_position
-        qinput[neg_position] = 0 # The zero in ieee754 is all zero as well.
+        qinput[neg_position] = 0  # The zero in ieee754 is all zero as well.
         return qinput, qinput_config
 
     @staticmethod
--- pimfixedpoint/quantization.py
+++ pimfixedpoint/quantization.py
@@ -1,11 +1,258 @@
 from __future__ import annotations
 
 import torch
+from torch import Tensor
 import numpy as np
-from commonMethod import get_fixed_point_position, pow_2_n, get_pos_bit_width, get_neg_bit_width, \
-    print_call_abstract_method_info
 import math
 
+from enum import Enum
+
+
+class TensorType(Enum):
+    Normal = 0
+    Ref = 1
+    PN = 2
+
+
+def get_fixed_point_position(max_abs: float, bit_width: int) -> int:
+    return math.ceil(math.log2(max_abs / ((1 << (bit_width - 1)) - 1)))
+
+
+def pow_2_n(n: int) -> int:
+    return 1 << n
+
+
+def get_pos_bit_width(pos_int: int):
+    return math.ceil(math.log2(pos_int + 1)) + 1
+
+
+def get_neg_bit_width(neg_int: int):
+    return math.ceil(math.log2(-neg_int)) + 1
+
+
+def parse_quantization_para(quantization_para: Tensor):
+    s = quantization_para[0].item()
+    bit_width = quantization_para[1].item()
+    tensor_type = TensorType(quantization_para[2].item())
+
+    return s, bit_width, tensor_type
+
+
+def print_quantization_info(s: int, bit_width: int):
+    resolution = pow(2, s)
+    neg_levels = pow_2_n(bit_width - 1)
+    pos_levels = neg_levels - 1
+    max_value = pos_levels * resolution
+    min_value = -neg_levels * resolution
+
+    print(f'fixed point position: {s}\n'
+          f'quantization resolution: {resolution}\n'
+          f'bit width: {bit_width}\n'
+          f'neg levels: {neg_levels}\n'
+          f'pos levels: {pos_levels}\n'
+          f'data range: {min_value} ~ {max_value}')
+
+
+def int_to_float(int_tensor: Tensor) -> Tensor:
+    return torch.from_numpy(int_tensor.numpy().view(dtype=np.float32))
+
+
+def float_to_int(float_tensor: Tensor) -> Tensor:
+    return torch.from_numpy(float_tensor.numpy().view(dtype=np.int32))
+
+
+def parse_float_tensor_list(float_tensor_list: list):
+    if float_tensor_list[0] is not None:
+        int_tensor = float_to_int(float_tensor_list[0])
+    else:
+        int_tensor = None
+
+    quantization_para_tensor = float_to_int(float_tensor_list[1])
+
+    return int_tensor, quantization_para_tensor
+
+
+def creat_quantization_para(s: int = None, bit_width: int = None, tensor_type: TensorType = None):
+    quantization_para = torch.empty(3, dtype=torch.int32)
+    if s is not None:
+        quantization_para[0] = s
+    if bit_width is not None:
+        quantization_para[1] = bit_width
+    if tensor_type is not None:
+        quantization_para[2] = tensor_type.value
+
+    return int_to_float(quantization_para)
+
+
+# return int_to_float(quantization_tensor), quantization_para can be modified in this method, don't need to return
+def quantization_tensor(quantization_para: Tensor, tensor: Tensor, max_abs_value: float) -> torch.Tensor:
+    quantization_para = float_to_int(quantization_para)
+    _, bit_width, tensor_type = parse_quantization_para(quantization_para)  # s is unknown
+
+    if tensor_type == TensorType.Normal:
+        s = get_fixed_point_position(max_abs_value, bit_width)
+        quantization_para[0] = s
+        resolution = pow(2, s)
+        int_tensor = tensor.div(resolution).round().to(torch.int32)
+    elif tensor_type == TensorType.Ref:
+        s = get_fixed_point_position(max_abs_value, bit_width)
+        quantization_para[0] = s
+        resolution = pow(2, s)
+        int_tensor = torch.empty([tensor.size()[0], tensor.size()[1] + 1], dtype=torch.int32)
+        neg_levels = pow_2_n(bit_width - 1)
+        int_tensor[:, -1] = neg_levels
+        int_tensor[:, 0:-1] = tensor.div(resolution).round().to(torch.int32).add(neg_levels)
+    elif tensor_type == TensorType.PN:
+        raise Exception("We don't implement this tensor_type!", tensor_type)
+    else:
+        raise Exception("Invalid tensor_type!", tensor_type)
+
+    return int_to_float(int_tensor)
+
+
+def de_quantization(float_tensor_list: list) -> Tensor:
+    int_tensor, quantization_para = parse_float_tensor_list(float_tensor_list)
+    s, bit_width, tensor_type = parse_quantization_para(quantization_para)
+
+    if tensor_type == TensorType.Normal:
+        resolution = pow(2, s)
+
+        return int_tensor.mul(resolution)
+    elif tensor_type == TensorType.Ref:
+        resolution = pow(2, s)
+        neg_levels = pow_2_n(bit_width - 1)
+
+        return int_tensor[:, 0:-1].sub(neg_levels).mul(resolution)
+    elif tensor_type == TensorType.PN:
+        raise Exception("We don't implement this tensor_type!", tensor_type)
+    else:
+        raise Exception("Invalid tensor_type!", tensor_type)
+
+
+def change_bit_width_(float_tensor_list: list, new_bit_width):
+    int_tensor, quantization_para = parse_float_tensor_list(float_tensor_list)
+    s, bit_width, tensor_type = parse_quantization_para(quantization_para)
+
+    assert (tensor_type == TensorType.Normal)
+
+    if new_bit_width < bit_width:
+        quantization_para[0] = s + bit_width - new_bit_width
+        int_tensor.__irshift__(bit_width - new_bit_width)
+
+    quantization_para[1] = new_bit_width
+
+
+# not in-situ method, delete _
+def add_additional_one(float_tensor_list: list) -> Tensor:
+    int_tensor, quantization_para = parse_float_tensor_list(float_tensor_list)
+    s, bit_width, tensor_type = parse_quantization_para(quantization_para)
+
+    assert (tensor_type == TensorType.Normal)
+
+    resolution = pow(2, s)
+    max_value = (pow_2_n(bit_width) - 1) * resolution
+
+    int_one = round(1 / resolution)
+
+    if max_value < 1:
+        new_bit_width = get_pos_bit_width(int_one)
+        change_bit_width_(float_tensor_list, new_bit_width)
+
+    full_one_col = torch.full([int_tensor.size()[0], 1], int_one, dtype=torch.int32)
+    int_tensor = torch.cat((int_tensor, full_one_col), 1)
+
+    return int_to_float(int_tensor)
+
+
+# don't need?
+def remove_additional_one_(float_tensor: Tensor):
+    float_tensor = float_tensor[:, 0:-1]
+
+    return float_tensor
+
+
+def write_array(array_tensor_list: list, data_tensor_list: list) -> Tensor:
+    array_int_tensor, array_quantization_para = parse_float_tensor_list(array_tensor_list)
+    _, array_bit_width, array_tensor_type = parse_quantization_para(array_quantization_para)
+    data_int_tensor, data_quantization_para = parse_float_tensor_list(data_tensor_list)
+    data_s, data_bit_width, data_tensor_type = parse_quantization_para(data_quantization_para)
+
+    assert (array_tensor_type == TensorType.Ref or array_tensor_type == TensorType.PN)
+    assert (data_tensor_type == TensorType.Normal)
+
+    if array_tensor_type == TensorType.Ref:
+        neg_levels = pow_2_n(array_bit_width - 1)
+        if array_int_tensor is None:
+            array_int_tensor = \
+                torch.empty([data_int_tensor.size()[0], data_int_tensor.size()[1] + 1], dtype=torch.int32)
+            array_int_tensor[:, -1] = neg_levels
+
+        if array_bit_width >= data_bit_width:
+            array_quantization_para[0] = data_s
+            array_int_tensor[:, 0:-1] = data_int_tensor.add(neg_levels)
+        else:
+            array_quantization_para[0] = data_s + data_bit_width - array_bit_width
+            array_int_tensor[:, 0:-1] = data_int_tensor.__rshift__(data_bit_width - array_bit_width).add(neg_levels)
+
+    else:
+        raise Exception("We don't implement this tensor_type!", array_tensor_type)
+
+    return int_to_float(array_int_tensor)
+
+
+def set_appropriate_bit_width_(float_tensor_list: list):
+    int_tensor, quantization_para = parse_float_tensor_list(float_tensor_list)
+    _, _, tensor_type = parse_quantization_para(quantization_para)
+
+    assert (tensor_type == TensorType.Normal)
+
+    max_int = int_tensor.max().item()
+    min_int = int_tensor.min().item()
+
+    if max_int <= 0:
+        quantization_para[1] = max(get_neg_bit_width(min_int), 1)
+    elif min_int >= 0:
+        quantization_para[1] = get_pos_bit_width(max_int)
+    else:
+        quantization_para[1] = max(get_pos_bit_width(max_int), get_neg_bit_width(min_int))
+
+
+def normal_matmul_array(normal_tensor_list: list, array_tensor_list: list) -> [torch.Tensor, torch.Tensor]:
+    normal_int_tensor, normal_quantization_para = parse_float_tensor_list(normal_tensor_list)
+    normal_s, normal_bit_width, normal_tensor_type = parse_quantization_para(normal_quantization_para)
+    array_int_tensor, array_quantization_para = parse_float_tensor_list(array_tensor_list)
+    array_s, array_bit_width, array_tensor_type = parse_quantization_para(array_quantization_para)
+
+    assert (normal_tensor_type == TensorType.Normal)
+    assert (array_tensor_type == TensorType.Ref or array_tensor_type == TensorType.PN)
+
+    if array_tensor_type == TensorType.Ref:
+        temp_result = normal_int_tensor.matmul(array_int_tensor)
+        matmul_result = temp_result[:, 0:-1] - temp_result[:, -1].unsqueeze(0).t()
+        matmul_result_quantization_para = creat_quantization_para(s=normal_s + array_s, tensor_type=TensorType.Normal)
+        set_appropriate_bit_width_([matmul_result, matmul_result_quantization_para])
+    else:
+        raise Exception("We don't implement this tensor_type!", array_tensor_type)
+
+    return matmul_result, matmul_result_quantization_para
+
+
+def normal_t_matmul_array(normal_tensor_list: list, array_tensor_list: list) -> [torch.Tensor, torch.Tensor]:
+    normal_tensor, normal_para = normal_tensor_list
+
+    return normal_matmul_array([normal_tensor.t(), normal_para], array_tensor_list)
+
+
+def quantization_tensor_less(float_tensor_list: list, a: float) -> torch.BoolTensor:
+    int_tensor, quantization_para = parse_float_tensor_list(float_tensor_list)
+    s, _, tensor_type = parse_quantization_para(quantization_para)
+
+    if tensor_type == TensorType.Normal:
+        int_a = round(a / pow(2, s))
+
+        return int_tensor < int_a
+    else:
+        raise Exception("We don't implement this tensor_type!", tensor_type)
 
 # quantization parameter
 class QTensor(torch.Tensor):
@@ -40,12 +287,25 @@ class QTensor(torch.Tensor):
     #     pass
     #
     def de_quantization(self):
-        print_call_abstract_method_info()
+        # print_call_abstract_method_info()
         pass
 
     def bind_fixed_tensor(self):
         self.data = torch.from_numpy(self.fixed_tensor.numpy().view(dtype=np.float32))
 
+    def set_appropriate_bit_width(self):
+        max_int = self.fixed_tensor.max().item()
+        min_int = self.fixed_tensor.min().item()
+        if max_int <= 0:
+            self.bit_width = max(get_neg_bit_width(min_int), 1)
+        elif min_int >= 0:
+            self.bit_width = get_pos_bit_width(max_int)
+        else:
+            self.bit_width = max(get_pos_bit_width(max_int), get_neg_bit_width(min_int))
+        self.neg_levels = pow_2_n(self.bit_width - 1)
+        self.pos_levels = self.neg_levels - 1
+        self.min_value = -self.resolution * self.neg_levels
+        self.max_value = self.resolution * self.pos_levels
 
 class ArrayTensor(QTensor):
     def __init__(self, bit_width: int):
@@ -57,15 +317,15 @@ class ArrayTensor(QTensor):
 
     # may change later
     def sub_normal(self, other: NormalTensor):
-        print_call_abstract_method_info()
+        # print_call_abstract_method_info()
         pass
 
     def write_normal(self, other: NormalTensor):
-        print_call_abstract_method_info()
+        # print_call_abstract_method_info()
         pass
 
     def mul_num(self, alpha: float, alpha_bit_width: int) -> NormalTensor:
-        print_call_abstract_method_info()
+        # print_call_abstract_method_info()
         return NormalTensor()
 
 
@@ -227,7 +487,7 @@ class RefTensor(ArrayTensor):
             other_data = other.fixed_tensor.__rshift__(shift)
         else:
             other_data = other.fixed_tensor.__lshift__(-shift)
-        
+
         data -= other_data
         data[data < 0] = 0
         data[data > self.max_int_number] = self.max_int_number
@@ -239,7 +499,7 @@ class RefTensor(ArrayTensor):
             other_data = other.fixed_tensor.__rshift__(shift).t()
         else:
             other_data = other.fixed_tensor.__lshift__(-shift).t()
-        
+
         data -= other_data
         data[data < 0] = 0
         data[data > self.max_int_number] = self.max_int_number

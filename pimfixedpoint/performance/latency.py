74995dcee44b9392956976384d1d5ffd227755b7
Cheng Huan <1556673504@qq.com>
2023-03-29 19:50:10 +0800

    unstable version

--- pimfixedpoint/performance/latency.py
+++ pimfixedpoint/performance/latency.py
@@ -9,37 +9,39 @@ from VGG_cifar10_model import fp_vgg16
 
 class latencyModule:
     def __init__(self, net: nn.Module):
-        # self.layer_num = 0
-        # for layer in net.modules():
-        #     if hasattr(layer, 'weightBits'):
-        #         self.layer_num += 1
-
         self.net = net
         self.all_latency = 0.0
+        self.stage_latency_list = self.produce_stage_latency_list()
         self.calculate_latency()
 
     def calculate_latency(self):
         for epoch_index in range(para.epochs):
             for iter_index in range(para.iterations):
-                self.all_latency += self.calculate_iter_latency(iter_index)
+                self.all_latency += self.calculate_iter_latency(epoch_index, iter_index)
 
-    def calculate_iter_latency(self, iter_index):
+    def calculate_iter_latency(self, epoch_index, iter_index):
+        latency = 0
         iter_data_size = para.data_size_in_iter[iter_index]
 
-        stage_latency_list = self.produce_stage_latency_list()
+        # stage_latency_list = self.produce_stage_latency_list()
 
         if para.compute_mode == 'pipeline':
-            max_stage_latency = max(stage_latency_list)
-            self.all_latency += (len(stage_latency_list) + iter_data_size - 1) * max_stage_latency
+            max_stage_latency = max(self.stage_latency_list)
+            latency += (len(self.stage_latency_list) + iter_data_size - 1) * max_stage_latency
         elif para.compute_mode == 'sequential':
-            self.all_latency += sum(stage_latency_list) * iter_data_size
+            latency += sum(self.stage_latency_list) * iter_data_size
         else:
             raise Exception('illegal compute mode: ' + para.compute_mode)
-        return iter_index
+
+        if para.training:
+            update_latency = self.calculate_update_latency(epoch_index, iter_index)
+            latency += update_latency
+
+        return latency
 
     def produce_stage_latency_list(self):
         stage_latency_list = []
-        previous_stage = []  # may include multiple layers: cnn/fc + relu(op) + pool(op)
+        previous_stage = []  # may include multiple layers: cnn/fc + relu(op) + pool(op)...
 
         # forward
         for layer in self.net.modules():
@@ -79,7 +81,8 @@ class latencyModule:
         return stage_latency_list
 
     def calculate_stage_latency(self, stage, backward=False):
-        latency = 0
+        latency = 0.
+
         if backward:
             # backward latency:
             # Todo:
@@ -97,14 +100,24 @@ class latencyModule:
             # transfer activation latency (to next layer, for backward)
             # Todo:
             for layer in stage:
-                if isinstance(layer, nn.Flatten):
-                    pass
-                elif isinstance(layer, fpnn.Quan):
-                    pass
-                elif isinstance(layer, fpnn.Linear):
+                if isinstance(layer, fpnn.Linear):
                     latency += 1.
-                    pass
-                pass
+                elif isinstance(layer, fpnn.ReLU):
+                    latency += 2.
+                elif isinstance(layer, fpnn.Dropout):
+                    latency += 3.0
+                elif isinstance(layer, nn.MaxPool2d):
+                    latency += 4.0
+                elif isinstance(layer, fpnn.Conv2d):
+                    latency += 5.0
+                elif isinstance(layer, nn.ReLU):
+                    latency += 6.0
+                elif not isinstance(layer, nn.Flatten) \
+                        and not isinstance(layer, fpnn.Quan)\
+                        and not isinstance(layer, fpnn.DeQuan):
+                    # don't support bn layer now
+                    raise Exception('illegal layer type' + layer.__class__.__name__)
+
             return 2.
 
     def calculate_loss_latency(self):
@@ -117,7 +130,7 @@ class latencyModule:
 
         return loss_latency
 
-    def calculate_update_latency(self, module):
+    def calculate_update_latency(self, epoch_index, iter_index):
         if para.training:
             # todo:
             return 1.
